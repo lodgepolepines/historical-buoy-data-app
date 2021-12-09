a20e47b845afc20eec891b516208b21478232c14
lodgepolepines <53026628+lodgepolepines@users.noreply.github.com>
2021-12-09 15:16:19 -0500

    update title

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ from selenium.webdriver.common.by import By
 import urllib.error
 
 st.set_page_config(page_title='Historic Buoy Data App v0.1')
-st.title('Historic Buoy Data App v0.1')
+st.title('Historic Buoy Data App')
 
 # starting data
 data = pd.DataFrame({
